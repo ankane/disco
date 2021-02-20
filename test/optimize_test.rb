93222e8965bbf03b87a5ec983bb98612aa551d9f
Andrew Kane <andrew@ankane.org>
2021-02-19 21:26:04 -0800

    Moved optimize tests to new file

---
+++ test/optimize_test.rb
@@ -0,0 +1,92 @@
+require_relative "test_helper"
+
+class OptimizeTest < Minitest::Test
+  def setup
+    skip "Not available on Windows" if windows?
+  end
+
+  def test_optimize_user_recs
+    data = Disco.load_movielens
+    recommender = Disco::Recommender.new(factors: 20)
+    recommender.fit(data)
+
+    original_recs = recommender.user_recs(1)
+
+    recommender.optimize_user_recs
+
+    recs = recommender.user_recs(1)
+    assert_equal original_recs.map { |v| v[:item_id] }, recs.map { |v| v[:item_id] }
+    original_recs.zip(recs).each do |exp, act|
+      assert_in_delta exp[:score], act[:score]
+    end
+    assert_equal 5, recs.size
+  end
+
+  def test_optimize_item_recs
+    data = Disco.load_movielens
+    recommender = Disco::Recommender.new(factors: 20)
+    recommender.fit(data)
+
+    original_recs = recommender.item_recs("Star Wars (1977)")
+
+    recommender.optimize_item_recs(library: "faiss")
+
+    recs = recommender.item_recs("Star Wars (1977)")
+    assert_equal original_recs.map { |v| v[:item_id] }, recs.map { |v| v[:item_id] }
+    original_recs.zip(recs).each do |exp, act|
+      assert_in_delta exp[:score], act[:score]
+    end
+    assert_equal 5, recs.size
+
+    item_ids = recs.map { |r| r[:item_id] }
+    assert_includes item_ids, "Empire Strikes Back, The (1980)"
+    assert_includes item_ids, "Return of the Jedi (1983)"
+
+    assert_in_delta 0.9972, recs.first[:score], 0.01
+  end
+
+  def test_optimize_similar_users
+    data = Disco.load_movielens
+    recommender = Disco::Recommender.new(factors: 20)
+    recommender.fit(data)
+
+    original_recs = recommender.similar_users(1)
+
+    recommender.optimize_similar_users(library: "faiss")
+
+    recs = recommender.similar_users(1)
+
+    assert_equal original_recs.map { |v| v[:item_id] }, recs.map { |v| v[:item_id] }
+    original_recs.zip(recs).each do |exp, act|
+      assert_in_delta exp[:score], act[:score]
+    end
+    assert_equal 5, recs.size
+  end
+
+  def test_optimize_item_recs_ngt
+    data = Disco.load_movielens
+    recommender = Disco::Recommender.new(factors: 20)
+    recommender.fit(data)
+
+    original_recs = recommender.item_recs("Star Wars (1977)")
+
+    recommender.optimize_item_recs(library: "ngt")
+
+    recs = recommender.item_recs("Star Wars (1977)")
+    assert_equal original_recs.map { |v| v[:item_id] }, recs.map { |v| v[:item_id] }
+    original_recs.zip(recs).each do |exp, act|
+      assert_in_delta exp[:score], act[:score]
+    end
+    assert_equal 5, recs.size
+
+    item_ids = recs.map { |r| r[:item_id] }
+    assert_includes item_ids, "Empire Strikes Back, The (1980)"
+    assert_includes item_ids, "Return of the Jedi (1983)"
+
+    assert_in_delta 0.9972, recs.first[:score], 0.01
+  end
+
+  def windows?
+    Gem.win_platform?
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,9 +8,3 @@ require "daru"
 require "rover"
 
 require_relative "support/active_record"
-
-class Minitest::Test
-  def windows?
-    Gem.win_platform?
-  end
-end
